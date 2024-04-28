bbe6fdc00d99f2fd38dbbf3bb75e7a10421c6a3e
IvanSlivenko <ivan8822@ukr.net>
2024-04-28 12:19:26 +0300

    28-04-2024-12-19

--- dev_env/app1/main/views.py
+++ dev_env/app1/main/views.py
@@ -1,9 +1,24 @@
-import http
+# import http
+# from turtle import title
 from django.http import HttpResponse
 from django.shortcuts import render
 
 def index(request):
-    return HttpResponse('Home page')
+    # return HttpResponse('Home page')
+    context={
+        'title': 'Home',
+        'content':'Головна сторінка магазину - HOME',
+        'list':['fsrst','second'],
+        'dict':{'first':1},
+        'is_authenticated': False 
+    }
+    return render(request, 'main/index.html', context)
 
 def about(request):
-    return HttpResponse('About page')
+    # return HttpResponse('About page')
+    context={
+        'title': 'Про нас',
+        'content':' Тут буде написано текст про нас'    
+        }
+    return render(request,'main/about.html', context)
+
