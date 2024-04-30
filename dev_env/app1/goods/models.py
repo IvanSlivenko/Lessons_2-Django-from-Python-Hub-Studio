f809c6ce2f5955ab7d7a564c20834592fe57b8cd
IvanSlivenko <ivan8822@ukr.net>
2024-04-30 16:18:32 +0300

    30-04-2024-16-18

--- dev_env/app1/goods/models.py
+++ dev_env/app1/goods/models.py
@@ -1,9 +1,30 @@
 from django.db import models
 
 class Categories(models.Model):
-    name = models.CharField(max_length=150, unique=True)
-    slug = models.SlugField(max_length=200, unique=True, blank=True, null=True)
+    name = models.CharField(max_length=150, unique=True, verbose_name="Ім'я")
+    slug = models.SlugField(max_length=200, unique=True, blank=True, null=True, verbose_name='URL')
 
 
     class Meta():
         db_table = 'category'
+        verbose_name ='Категорію'
+        verbose_name_plural = 'Категорії'
+
+class Products(models.Model):
+    name = models.CharField(max_length=150, unique=True, verbose_name="Ім'я")
+    slug = models.SlugField(max_length=200, unique=True, blank=True, null=True, verbose_name='URL')
+    description = models.TextField(blank=True, null=True, verbose_name="Опис")
+    image =  models.ImageField(upload_to='goods_images', blank=True, null=True, verbose_name='Зображення')
+    price = models.DecimalField(default=0.00, max_digits=7, decimal_places=2, verbose_name='Ціна')
+    discount = models.DecimalField(default=0.00, max_digits=7, decimal_places=2, verbose_name='Знижка у %')
+    quantity = models.PositiveIntegerField(default=0,  verbose_name='Кількість')
+    category = models.ForeignKey(to=Categories, on_delete=models.PROTECT, verbose_name='Категорія')
+
+    
+
+
+
+    class Meta():
+        db_table = 'product'
+        verbose_name ='Продукт'
+        verbose_name_plural = 'Продукти'
--- dev_env/app1/main/admin.py
+++ dev_env/app1/main/admin.py
@@ -2,6 +2,3 @@ from django.contrib import admin
 
 # Register your models here.
 
-from goods.models import Categories
-
-admin.site.register(Categories)
