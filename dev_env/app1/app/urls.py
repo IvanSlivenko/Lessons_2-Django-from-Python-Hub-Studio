10e882fb7d016cfe03b8309415024668c89894b2
IvanSlivenko <ivan8822@ukr.net>
2024-05-03 17:01:20 +0300

    03-05-2024-17-01

--- dev_env/app1/app/urls.py
+++ dev_env/app1/app/urls.py
@@ -16,7 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
-from app.settings import DEBUG
+from django.conf.urls.static import static
+from app import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -26,10 +27,12 @@ urlpatterns = [
     
 ]
 
-if DEBUG:
+if settings.DEBUG:
     urlpatterns += [
         path("__debug__/", include("debug_toolbar.urls")),
         ]
+    
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     
 
 """path('catalog/product/', include('goods.urls', namespace='product')),"""
--- dev_env/app1/goods/views.py
+++ dev_env/app1/goods/views.py
@@ -7,84 +7,17 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from goods.models import Products
+
+
 
 def catalog(request):
+
+    goods = Products.objects.all()
+    
     context = {
         "title": "Каталог",
-        "goods": [
-            {
-                "image": "deps/images/goods/set of tea table and three chairs.jpg",
-                "name": "Чайний столик та три стільця",
-                "description": "Комплект з трьох стільців та дизайнерский столик для вітальні.",
-                "price": 150.00,
-            },
-            {
-                "image": "deps/images/goods/set of tea table and two chairs.jpg",
-                "name": "Чайный столик та два стільця",
-                "description": "Набір -  стіл та два стільці в мінімалістичному  стилі.",
-                "price": 93.00,
-            },
-            {
-                "image": "deps/images/goods/double bed.jpg",
-                "name": "Двоспальне ліжко",
-                "description": "Ліжко двухспальная з надголовником ортопедична.",
-                "price": 670.00,
-            },
-            {
-                "image": "deps/images/goods/kitchen table.jpg",
-                "name": "Кухонний стіл з раковиною",
-                "description": "Кухонний стіл з вбудованою раковиною та стільцями.",
-                "price": 365.00,
-            },
-            {
-                "image": "deps/images/goods/kitchen table 2.jpg",
-                "name": "Кухонний стіл з вбудованою раковиною",
-                "description": "Кухонный стол з вбудованою плитою та раковиною.",
-                "price": 430.00,
-            },
-            {
-                "image": "deps/images/goods/corner sofa.jpg",
-                "name": "Кутовий диван для вітальні",
-                "description": "Кутовий диван для затишної вітальні",
-                "price": 610.00,
-            },
-            {
-                "image": "deps/images/goods/bedside table.jpg",
-                "name": "Приліжковий столик",
-                "description": "Приліжковий столик з двома висувними ящиками.",
-                "price": 55.00,
-            },
-            {
-                "image": "deps/images/goods/sofa.jpg",
-                "name": "Диван звичайний",
-                "description": "Диван, він же софа звичайна.",
-                "price": 190.00,
-            },
-            {
-                "image": "deps/images/goods/office chair.jpg",
-                "name": "Стілець офісний",
-                "description": "Зручний стілець офісний ",
-                "price": 30.00,
-            },
-            {
-                "image": "deps/images/goods/plants.jpg",
-                "name": "Рослина",
-                "description": "Рослина для інтер'єру.",
-                "price": 10.00,
-            },
-            {
-                "image": "deps/images/goods/flower.jpg",
-                "name": "Квітка стилізована",
-                "description": "Дизайнерська квітка для створення затишку в інтер'єрі.",
-                "price": 15.00,
-            },
-            {
-                "image": "deps/images/goods/strange table.jpg",
-                "name": "Приліжковий столик",
-                "description": "Столик для спальні.",
-                "price": 25.00,
-            },
-        ],
+        "goods": goods,
     }
     return render(request, "goods/catalog.html", context)
 
