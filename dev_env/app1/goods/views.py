6fe9e4e69942c823797e2472080fc3e85047d2a0
IvanSlivenko <ivan8822@ukr.net>
2024-05-06 11:06:50 +0300

    06-05-2024-11-06

--- dev_env/app1/goods/views.py
+++ dev_env/app1/goods/views.py
@@ -1,26 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, get_list_or_404, render
 
 # Create your views here.
 
 # import http
 # from turtle import title
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+from django.core.paginator import Paginator
 
 from goods.models import Products
 
 
 
-def catalog(request, category_slug):
+def catalog(request, category_slug, page=1):
 
     if category_slug == 'all':
         goods = Products.objects.all()
     else:    
-        goods = Products.objects.filter(category__slug=category_slug)
+        goods = get_list_or_404(Products.objects.filter(category__slug=category_slug))
+        
+
+    paginator = Paginator(goods, 3)
+    current_page= paginator.page(page)
     
     context = {
         "title": "Каталог",
-        "goods": goods,
+        "goods": current_page,
+        "slug_url" : category_slug
     }
     return render(request, "goods/catalog.html", context)
 
@@ -34,6 +40,46 @@ def product(request, product_slug ):
     # else:
         # product = Products.objects.get(slug=product_slug)
     product = Products.objects.get(slug=product_slug)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     
     context = {
         "title": "Product",
