9e49f5fecee657d2eab4778c345d17b639d9c4cf
IvanSlivenko <ivan8822@ukr.net>
2024-05-08 21:44:48 +0300

    08-05-2024-21-44

--- dev_env/app1/goods/urls.py
+++ dev_env/app1/goods/urls.py
@@ -22,11 +22,13 @@ from goods import views
 app_name ='goods'
 
 urlpatterns = [
-    # path('', views.catalog, name='index'),
-    path('<slug:category_slug>/', views.catalog, name='index'),
+    
     path('search/', views.catalog, name='search'),
+    path('<slug:category_slug>/', views.catalog, name='index'),
+    path('product/<slug:product_slug>/', views.product, name='product'),
 
+    # path('', views.catalog, name='index'),
     # path('<slug:category_slug>/<int:page>/', views.catalog, name='index'),
     # path('product/<int:product_id>/', views.product, name='product'),
-    path('product/<slug:product_slug>/', views.product, name='product'),
+    
 ]
