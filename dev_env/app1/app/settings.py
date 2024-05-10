f9a2c0965ce7e6d95d63d7e4dcd8b2c90f2f4a31
IvanSlivenko <ivan8822@ukr.net>
2024-05-10 22:56:07 +0300

    10-05-2024-22-56

--- dev_env/app1/app/settings.py
+++ dev_env/app1/app/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
 
     'main',
     'goods',
+    'users',
 ]
 
 MIDDLEWARE = [
@@ -65,7 +66,7 @@ ROOT_URLCONF = 'app.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
