57d1c77b9aa950d168aa258196f6036f2baa8e3a
IvanSlivenko <ivan8822@ukr.net>
2024-05-14 12:41:42 +0300

    14-05-2024-12-41

--- dev_env/app1/users/forms.py
+++ dev_env/app1/users/forms.py
@@ -1,5 +1,6 @@
+import email
 from django import forms
-from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 
 from users.models import User
 
@@ -15,21 +16,104 @@ from users.models import User
 #         fields = ['username', 'password']     
 #---------------------------------------------------------
 class UserLoginForm(AuthenticationForm):
+     class Meta:
+        model = User 
+        fields = [
+            "username",
+            'password',
+            ]           
     
-    username = forms.CharField(
-        widget=forms.TextInput(attrs={"autofocus": True,
-                                      "class":" form-control",
-                                      "placeholder": "Введіть ваше ім'я користувача",
-                                      })
-        )
+     username = forms.CharField()
+     password = forms.CharField()
+#------------------------------------------------------------------------------------------
+     # username = forms.CharField(
+    #     label = "Ім'я",
+    #     widget=forms.TextInput(attrs={"autofocus": True,
+    #                                   "class":" form-control",
+    #                                   "placeholder": "Введіть ваше ім'я користувача",
+    #                                   })
+    #     )
     
-    password = forms.CharField(
-        widget=forms.PasswordInput(attrs={"autocomplete": "current-password",
-                                          "class":" form-control",
-                                          "placeholder": "Введіть ваш пароль",
+    # password = forms.CharField(
+    #     label = "Пароль",
+    #     widget=forms.PasswordInput(attrs={"autocomplete": "current-password",
+    #                                       "class":" form-control",
+    #                                       "placeholder": "Введіть ваш пароль",
                                           
-                                          })
-    )
+    #                                       })
+    # )
+#--------------------------------------------------------------------------------------------
+class UserRegistrationForm(UserCreationForm):
     class Meta:
         model = User 
-        fields = ['username', 'password']           
+        fields = (
+            "first_name",
+            "last_name",
+            "username",
+            "email",
+            "password1",
+            "password2",
+            )
+        
+    first_name=forms.CharField()
+    last_name=forms.CharField()
+    username=forms.CharField()
+    email=forms.CharField()
+    password1=forms.CharField()
+    password2=forms.CharField()
+    
+#-----------------------------------------------------------------------------------------------     
+#     first_name = forms.CharField(
+#         widget=forms.TextInput(
+#             attrs={
+#                 "class":"form-control",
+#                 "placehjlder":"Введіть ваше ім'я",
+#             }
+#         )
+#     )  
+
+#     last_name = forms.CharField(
+#         widget=forms.TextInput(
+#             attrs={
+#                 "class":"form-control",
+#                 "placehjlder":"Введіть ваше прізвище",
+#             }
+#         )
+#     )   
+
+#     username = forms.CharField(
+#         widget=forms.TextInput(
+#             attrs={
+#                 "class":"form-control",
+#                 "placehjlder":"Введіть ваше ім'я користувача",
+#             }
+#         )
+#     )
+
+#     email = forms.CharField(
+#         widget=forms.EmailInput(
+#             attrs={
+#                 "class":"form-control",
+#                 "placehjlder":"Введіть вашу електронну пошту",
+#             }
+#         )
+#     )
+
+#     password1 = forms.CharField(
+#         widget=forms.PasswordInput(
+#             attrs={
+#                 "class":"form-control",
+#                 "placehjlder":"Введіть ваш пароль",
+#             }
+#         )
+#     )
+
+#     password2 = forms.CharField(
+#         widget=forms.PasswordInput(
+#             attrs={
+#                 "class":"form-control",
+#                 "placehjlder":"Підтвердіть ваш пароль",
+#             }
+#         )
+#     )  
+# #----------------------------------------------------------------------------
--- dev_env/app1/users/views.py
+++ dev_env/app1/users/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
 
-from users.forms import UserLoginForm
+from users.forms import UserLoginForm, UserRegistrationForm
 
 def login(request):
     if request.method == 'POST':
@@ -25,6 +25,15 @@ def login(request):
     return render(request, 'users/login.html', context)
 
 def registration(request):
+    if request.method == 'POST':
+        form = UserRegistrationForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            
+            return HttpResponseRedirect(reverse('main:index'))
+    else :
+        form = UserRegistrationForm()
+
     context = {
         'title': 'Home - Реєстрація'
     }
